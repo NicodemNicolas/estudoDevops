ecdb511b2741b60e36dfb69f6387591a451ab4e4
NicodemNicolas <166960211+NicodemNicolas@users.noreply.github.com>
2024-06-23 18:22:41 -0300

    Create data_teste.py

---
+++ data_teste.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+dataAtual = datetime.now()
+
+dataConvertida = dataAtual.strftime('%Y-%m-%d')
+horaAtual = dataAtual.strftime('%H:%M:%S') 
+
+print("Data atual: ", dataConvertida)
+print("Hora atual: ", horaAtual)
+print("Data atual: ", dataConvertida)
+print("Hora atual: ", horaAtual)
